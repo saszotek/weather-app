a399b125a57968ff814b9c44785a9c23e06d8a5f
saszotek <danielzielinski789@gmail.com>
2022-07-12 14:25:43 +0200

    validation

---
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/errormessage.scss";
+
+function ErrorMessage({ message }) {
+  return (
+    <div className="error-message-container">
+      <div className="error-message-container__info-box">
+        <h2>{message}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorMessage;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { serverURL } from "../../api/main";
 import "../../styles/header.scss";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 function Header({ setForecastData, setIsDisplay, setIsLoading }) {
   const [input, setInput] = useState("");
+  const [message, setMessage] = useState("");
+
   const inputReference = useRef(null);
 
   useEffect(() => {
@@ -21,6 +24,8 @@ function Header({ setForecastData, setIsDisplay, setIsLoading }) {
       })
       .catch((error) => {
         console.log(error);
+        setIsDisplay(false);
+        validationInput(error.response.data.error.code);
       });
   };
 
@@ -32,8 +37,40 @@ function Header({ setForecastData, setIsDisplay, setIsLoading }) {
     }
   };
 
+  const validationInput = (errorCode) => {
+    switch (errorCode) {
+      case 1002:
+        setMessage("API key not provided.");
+        break;
+      case 1003:
+        setMessage("Please enter a location.");
+        break;
+      case 1005:
+        setMessage("API request url is invalid");
+        break;
+      case 1006:
+        setMessage("No matching location found.");
+        break;
+      case 2006:
+        setMessage("API key provided is invalid");
+        break;
+      case 2007:
+        setMessage("Exceeded maximum number of calls per month.");
+        break;
+      case 2008:
+        setMessage("Authorization to the server has been revoked.");
+        break;
+      case 9999:
+        setMessage("Access to the forecast's data is unavailable for now.");
+        break;
+      default:
+        setMessage("Internal problem occured, try again later!");
+    }
+  };
+
   return (
     <div className="header-container">
+      {message && <ErrorMessage message={message} />}
       <div className="header-container__title">
         <h1>
           Weather <span>Forecast</span>
