6daf0b5019a9802ce4a938591ae37ab5b18b37eb
saszotek <danielzielinski789@gmail.com>
2022-07-19 11:19:35 +0200

    icon change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
+  useEffect(() => {
+    document.title = "Rainbow Forecast";
+  });
+
   return (
     <>
       <Dashboard />
