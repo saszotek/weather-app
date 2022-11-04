99e0674c620ff76ee9bbe9da4d13213be89adca9
saszotek <danielzielinski789@gmail.com>
2022-11-04 11:00:27 +0100

    dropdown menu impl

--- src/components/DropdownMenu/DropdownMenu.js
+++ src/components/DropdownMenu/DropdownMenu.js
@@ -6,32 +6,40 @@ function DropdownMenu({
   handleDropdownInput,
   handleDropdownDelete,
 }) {
-  return (
-    <div className={`dropdown-menu ${isOpen ? "active" : "inactive"}`}>
-      <ul>
-        {updatedLocations.map((updatedLocation, index) => {
-          return (
-            <li key={index}>
-              <div
-                onClick={() => {
-                  handleDropdownInput(updatedLocation);
-                }}
-              >
-                {updatedLocation}
-              </div>
-              <div
-                onClick={() => {
-                  handleDropdownDelete(index);
-                }}
-              >
-                X
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  if (!(updatedLocations.length === 0)) {
+    return (
+      <div className={`dropdown-menu ${isOpen ? "active" : "inactive"}`}>
+        <ul>
+          {updatedLocations.map((updatedLocation, index) => {
+            return (
+              <li key={index}>
+                <div
+                  onClick={() => {
+                    handleDropdownInput(updatedLocation);
+                  }}
+                >
+                  {updatedLocation}
+                </div>
+                <div
+                  onClick={() => {
+                    handleDropdownDelete(index);
+                  }}
+                >
+                  X
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className={`dropdown-menu ${isOpen ? "active" : "inactive"}`}>
+        <div className="dropdown-menu__empty">The list is empty</div>
+      </div>
+    );
+  }
 }
 
 export default DropdownMenu;
